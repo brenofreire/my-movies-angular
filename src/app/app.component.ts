09d53c44d4f2b4ccac6dba484be0884259a23412
Breno Freire <breno.loorran@gmail.com>
2019-10-24 21:43:23 -0300

    [Inicial] Template e CSS estático

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'desafio-tecnico-web';
+  url = 'http://br.web.img3.acsta.net/c_215_290/pictures/17/08/26/00/05/175443.jpg';
+  constructor(){
+    console.log('AAA');    
+  }
 }
