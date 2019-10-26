45da7f4f1eeabe1f3c881d81a5280275b5391997
Breno Freire <breno.loorran@gmail.com>
2019-10-26 19:51:04 -0300

    [CSS] Implementa o import do variáveis do Sass

--- src/app/components/movies-list/movies-list.component.ts
+++ src/app/components/movies-list/movies-list.component.ts
@@ -8,33 +8,11 @@ import { MoviesService } from 'src/services/movies/movies.service';
 })
 export class MoviesListComponent implements OnInit {
   public url = 'http://image.tmdb.org/t/p/w500';
-  private genres = [];
   @Input('movies') movies: Array<object>;
 
-  constructor(
-    private moviesServ: MoviesService,
-  ) { }
+  constructor() { }
 
   ngOnInit() {
-    this.getGenres();
-  }
-
-  objectKeys(obj: {}): Array<string> {
-    return Object.keys(obj);
-  }
-  /**
-   * @description Busca e armazena os gêneros disponíveis;
-   * @returns Os gêneros disponíveis
-   */
-  async getGenres(): Promise<any> {
-    this.genres = await this.moviesServ.getGenres();
-  };
-  /**
-   * @description Baseado no id go gênero, retorna o nome dele
-   * @param id 
-   * @returns Nome do gênero
-   */
-  returnGenreName(id: number): string {
-    for (let genre of this.genres) if (genre.id == id) return genre.name;
+    
   }
 }
