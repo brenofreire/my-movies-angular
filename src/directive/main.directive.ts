bf127311781033bee54b497da378095ff59456a0
Breno Freire <breno.loorran@gmail.com>
2019-10-26 03:59:09 -0300

    [Modularização] Criação das diretivas do sistema

---
+++ src/directive/main.directive.ts
@@ -0,0 +1,38 @@
+import { Directive, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appBackground]'
+})
+export class BackgroundDirective {
+  constructor(elementRef: ElementRef) {
+    elementRef.nativeElement.style.backgroundColor = '#116193';
+  }
+}
+@Directive({
+  selector: '[appPadding]'
+})
+export class PaddingDirective {
+  constructor(elementRef: ElementRef) {
+    elementRef.nativeElement.style.position = 'absolute';
+    elementRef.nativeElement.style.left = '2%';
+    elementRef.nativeElement.style.maxWidth = '100%';
+  }
+}
+@Directive({
+  selector: '[appFull]'
+})
+export class FullDirective {
+  constructor(elementRef: ElementRef) {
+    elementRef.nativeElement.style.display = 'block';
+    elementRef.nativeElement.style.width = '100%';
+  }
+}
+@Directive({
+  selector: '[appCenter]'
+})
+export class CenterDirective {
+  constructor(elementRef: ElementRef) {
+    elementRef.nativeElement.style.display = 'flex';
+    elementRef.nativeElement.style.justifyContent = 'center';
+  }
+}
