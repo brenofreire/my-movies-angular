cd84a7c8b51bd6cbf31bb7084516d9a6805c6798
Breno Freire <breno.loorran@gmail.com>
2019-10-26 23:20:58 -0300

    [General] Cria pipe de formatação de data DD/MM/YYYY

---
+++ src/pipes/to-hour.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'toHour'
+})
+export class ToHourPipe implements PipeTransform {
+
+  transform(value: number): string {
+    if (value > 0 && value / 60 < 1) return value + ' Minutes';
+    else {
+      let hours = Math.floor(value / 60);  
+      let minutes = value % 60;
+      return `${hours}h ${minutes}min`; 
+    };
+  }
+}
