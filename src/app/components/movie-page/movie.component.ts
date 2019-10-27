5dc77f6830706b61ac7b1ca4d90148113b12df17
Breno Freire <breno.loorran@gmail.com>
2019-10-27 02:24:11 -0300

    [MoviePage] Implementa layout mobile

--- src/app/components/movie-page/movie.component.ts
+++ src/app/components/movie-page/movie.component.ts
@@ -30,15 +30,19 @@ export class MovieComponent implements OnInit {
     });
     this.getMovieInfo();
   }
+  /**
+   * @returns O objeto com as informações do filme
+   */
   async getMovieInfo(): Promise<any> {
     this.api.get(`/movie/${this.id}`, '&append_to_response=videos').then(response => {
       this.movie = <any>response;
       if (this.movie['videos']) this.getsTrailer();
-      console.log(this.movie);
       this.loading = false;
     });
   }
-  q
+  /**
+   * @description Atribui a infomações do trailer
+   */
   getsTrailer(): void {
     for (let link of this.movie['videos']['results']) {
       if (link['site'] == 'YouTube') this.url_trailer = 'http://www.youtube.com/embed/' + link['key'];
@@ -46,12 +50,23 @@ export class MovieComponent implements OnInit {
     }
     this.url_trailer = this.sanitize(this.url_trailer);
   }
+  /**
+   * @description Abre exceção para exibição de conteúdo vindo do youtube
+   * @param url A url do vídeo deseja confiar
+   */
   sanitize(url: string): any {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
-  objectKeys(obj: {}) {
+  /**
+   * @param obj É o objeto a ser mapeado
+   */
+  objectKeys(obj: {}): Array<string | number> {
     return Object.keys(obj);
   }
+  /**
+   * @description Mapeia os gêneros do filme em um array flat
+   * @param genres Array a ser mapeado
+   */
   getGenresId(genres: Array<object>) {
     return genres.map(item => item['id']);
   }
